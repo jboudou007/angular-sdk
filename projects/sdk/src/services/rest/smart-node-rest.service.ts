7b031b90bc5e8ab4cd27c4d47e0a7ebd093d497c
HSuite Network <info@hbarsuite.network>
2023-01-30 19:52:22 +0100

    added endpoint for dex charts

--- projects/sdk/src/services/rest/smart-node-rest.service.ts
+++ projects/sdk/src/services/rest/smart-node-rest.service.ts
@@ -67,6 +67,35 @@ export class SmartNodeRestService {
     });
   }
 
+  public async getPoolCharts(
+    poolId: string, 
+    type: 'hour' | 'day' | 'week',
+    from?: number,
+    limit?: number,
+    order?: 'asc' | 'desc'
+  ): Promise<any> {
+    return new Promise(async(resolve, reject) => {
+      try {
+        let response = await this.smartNodeNetworkService.getApiEndpoint(`pools/pool/${poolId}/chart/${type}`,
+        { params: { 
+          from: from,
+          limit: limit,
+          order: order
+        } });
+
+        let node = this.smartNodeNetworkService.getCurrentNode();
+
+        resolve({
+          function: 'getPoolCharts',
+          node: node,
+          data: response
+        });
+      } catch(error) {
+        reject(error);        
+      }
+    });
+  }
+
   public async calculateLaunchpadDiscount(launchpadId: string, accountId: string): Promise<any> {
     return new Promise(async(resolve, reject) => {
       try {
